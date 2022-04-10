d7cd1a6015273a73876afceffdbf6443d9b71e32
zixi0825 <sunzhaohe0825@gmail.com>
2022-04-10 15:21:11 +0800

    feat(metric): add column_enums metric

---
+++ datavines-metric/datavines-metric-plugins/datavines-metric-column-enums/src/main/java/io/datavines/metric/plugin/ColumnEnums.java
@@ -0,0 +1,49 @@
+package io.datavines.metric.plugin;
+
+import io.datavines.metric.api.MetricDimension;
+import io.datavines.metric.api.MetricType;
+import io.datavines.metric.plugin.base.BaseSingleTable;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ColumnEnums extends BaseSingleTable {
+
+    public ColumnEnums(){
+        configSet.add("enum_list");
+        REQUIRED_OPTIONS.add("enum_list");
+    }
+
+    @Override
+    public String getName() {
+        return "column_enums";
+    }
+
+    @Override
+    public MetricDimension getDimension() {
+        return MetricDimension.EFFECTIVENESS;
+    }
+
+    @Override
+    public MetricType getType() {
+        return MetricType.SINGLE_TABLE;
+    }
+
+    @Override
+    public boolean isInvalidateItemsCanOutput() {
+        return true;
+    }
+
+    @Override
+    public void prepare(Map<String, String> config) {
+        if (config.containsKey("enum_list") && config.containsKey("column")) {
+            filters.add(" (${column} not in ( ${enum_list} ) or ${column} is null) ");
+        }
+        super.prepare(config);
+    }
+
+    @Override
+    public Set<String> getConfigSet() {
+        return configSet;
+    }
+}
