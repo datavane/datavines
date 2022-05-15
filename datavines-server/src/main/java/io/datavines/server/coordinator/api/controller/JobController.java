4cac12bb883a62f3927a22f94304a4b70f95989a
LT <yyyprettyboy@163.com>
2022-05-15 10:57:17 +0800

    edit JobController,add properties

--- datavines-server/src/main/java/io/datavines/server/coordinator/api/controller/JobController.java
+++ datavines-server/src/main/java/io/datavines/server/coordinator/api/controller/JobController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class JobController {
     private JobService jobService;
 
     @ApiOperation(value = "Job create")
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Object createJob(@Valid @RequestBody JobCreate jobCreate) {
         return jobService.createJob(jobCreate);
     }
