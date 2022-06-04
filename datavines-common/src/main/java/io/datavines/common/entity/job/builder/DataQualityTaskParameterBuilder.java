903c5d2d0535e24f86bd99a6b4eb53021c92f5a0
zixi0825 <sunzhaohe0825@gmail.com>
2022-06-04 22:53:18 +0800

    [Fix][Server] Add Header

--- datavines-common/src/main/java/io/datavines/common/entity/job/builder/DataQualityTaskParameterBuilder.java
+++ datavines-common/src/main/java/io/datavines/common/entity/job/builder/DataQualityTaskParameterBuilder.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.datavines.common.entity.job.builder;
 
 import io.datavines.common.entity.ConnectionInfo;
