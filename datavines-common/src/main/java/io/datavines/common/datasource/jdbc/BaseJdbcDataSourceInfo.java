75a10bbcd21decfbd1fe2bd75a3f760980720f4f
Chris-playground <2011578668@qq.com>
2025-02-14 13:20:49 +0800

    [Improvement][DataSource] datasource allows load multi databases (#514)

--- datavines-common/src/main/java/io/datavines/common/datasource/jdbc/BaseJdbcDataSourceInfo.java
+++ datavines-common/src/main/java/io/datavines/common/datasource/jdbc/BaseJdbcDataSourceInfo.java
@@ -119,12 +119,10 @@ public abstract class BaseJdbcDataSourceInfo {
      * @param jdbcUrl jdbc url
      */
     protected void appendDatabase(StringBuilder jdbcUrl) {
-        if (StringUtils.isNotEmpty(getDatabase())) {
-            if (getAddress().lastIndexOf('/') != (jdbcUrl.length() - 1)) {
-                jdbcUrl.append("/");
-            }
-            jdbcUrl.append(getDatabase());
+        if (getAddress().lastIndexOf('/') != (jdbcUrl.length() - 1)) {
+            jdbcUrl.append("/");
         }
+        jdbcUrl.append(getDatabase());
     }
 
     /**
