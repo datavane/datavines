d28177d1ba9ffe87cb14993802969bd6f0912fd9
zixi0825 <sunzhaohe0825@gmail.com>
2026-04-06 16:14:58 +0800

    [Refactor][Engine] Refactor engine module

--- datavines-engine/datavines-engine-plugins/datavines-engine-flink/datavines-engine-flink-core/src/main/java/io/datavines/engine/flink/core/FlinkDataVinesBootstrap.java
+++ datavines-engine/datavines-engine-plugins/datavines-engine-flink/datavines-engine-flink-core/src/main/java/io/datavines/engine/flink/core/FlinkDataVinesBootstrap.java
@@ -16,10 +16,27 @@
  */
 package io.datavines.engine.flink.core;
 
-import java.util.Base64;
-
+import io.datavines.common.config.Config;
+import io.datavines.common.config.DataVinesJobConfig;
+import io.datavines.engine.api.component.Component;
+import io.datavines.engine.api.env.RuntimeEnvironment;
 import io.datavines.engine.core.BaseDataVinesBootstrap;
+import io.datavines.engine.core.enums.ConnectorType;
+import io.datavines.engine.flink.api.FlinkRuntimeEnvironment;
+import io.datavines.engine.flink.api.stream.FlinkStreamSink;
+import io.datavines.engine.flink.api.stream.FlinkStreamSource;
+import io.datavines.engine.flink.jdbc.sink.JdbcSink;
+import io.datavines.engine.flink.jdbc.sink.MySQLSink;
+import io.datavines.engine.flink.jdbc.source.JdbcSource;
+import io.datavines.engine.flink.transform.sql.SqlTransform;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.datavines.engine.api.EngineConstants.PLUGIN_TYPE;
+import static io.datavines.engine.api.EngineConstants.TYPE;
 
 @Slf4j
 public class FlinkDataVinesBootstrap extends BaseDataVinesBootstrap {
@@ -32,5 +49,62 @@ public class FlinkDataVinesBootstrap extends BaseDataVinesBootstrap {
             args[0] = new String(Base64.getDecoder().decode(arg));
             bootstrap.execute(args);
         }
+    }
+
+    @Override
+    protected RuntimeEnvironment createRuntimeEnvironment(DataVinesJobConfig config) {
+        FlinkRuntimeEnvironment runtimeEnvironment = new FlinkRuntimeEnvironment();
+        Config runtimeConfig = new Config(config.getEnvConfig().getConfig());
+        runtimeConfig.put(TYPE, config.getEnvConfig().getType());
+        runtimeEnvironment.setConfig(runtimeConfig);
+        runtimeEnvironment.prepare();
+        return runtimeEnvironment;
+    }
+
+    @Override
+    protected List<Component> createSources(DataVinesJobConfig config) {
+        return config.getSourceParameters().stream().map(sourceConfig -> {
+            FlinkStreamSource source;
+            switch (ConnectorType.of(sourceConfig.getPlugin())) {
+                case JDBC:
+                    source = new JdbcSource();
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported flink source plugin: " + sourceConfig.getPlugin());
+            }
+            sourceConfig.getConfig().put(PLUGIN_TYPE, sourceConfig.getType());
+            source.setConfig(new Config(sourceConfig.getConfig()));
+            return (Component) source;
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    protected List<Component> createTransforms(DataVinesJobConfig config) {
+        return config.getTransformParameters().stream().map(transformConfig -> {
+            SqlTransform transform = new SqlTransform();
+            transformConfig.getConfig().put(PLUGIN_TYPE, transformConfig.getType());
+            transform.setConfig(new Config(transformConfig.getConfig()));
+            return (Component) transform;
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    protected List<Component> createSinks(DataVinesJobConfig config) {
+        return config.getSinkParameters().stream().map(sinkConfig -> {
+            FlinkStreamSink sink;
+            switch (ConnectorType.of(sinkConfig.getPlugin())) {
+                case JDBC:
+                    sink = new JdbcSink();
+                    break;
+                case MYSQL:
+                    sink = new MySQLSink();
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported flink sink plugin: " + sinkConfig.getPlugin());
+            }
+            sinkConfig.getConfig().put(PLUGIN_TYPE, sinkConfig.getType());
+            sink.setConfig(new Config(sinkConfig.getConfig()));
+            return (Component) sink;
+        }).collect(Collectors.toList());
     }
 }
